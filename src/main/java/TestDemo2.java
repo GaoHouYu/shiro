5bcfa72fd5c1df4b9e42f882c2b1de33ffeed0cf
GaoHouYu <452595846@qq.com>
2022-02-17 23:27:10 +0800

    first commit

---
+++ src/main/java/TestDemo2.java
@@ -0,0 +1,42 @@
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.crypto.hash.Md5Hash;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+
+
+public class TestDemo2 {
+
+
+    public static void main(String[] args) {
+        //获取一个SecurityManager工厂对象，加载shiro配置文件
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro_customer.ini");
+        //通过factory获取secutityManager对象
+        SecurityManager securityManager = factory.getInstance();
+        //将securityManager添加到运行时环境中
+        SecurityUtils.setSecurityManager(securityManager);
+        //通过SecurityManager获取Subject对象
+        Subject subject = SecurityUtils.getSubject();
+        //前端传入的用户密码
+        String loginName = "dm";
+        String passWord = "0616";
+        //第二个参数为盐值，第三个参数为加密次数
+        Md5Hash md5Hash = new Md5Hash(passWord,"aaa",9999);
+        System.out.println(md5Hash);
+        //将用户密码封装成一个token
+        AuthenticationToken token = new UsernamePasswordToken(loginName, passWord);
+        try {
+            subject.login(token);
+            System.out.println("成功登录！！");
+        } catch (UnknownAccountException e) {
+            System.out.println("账号不存在！");
+        } catch (IncorrectCredentialsException e) {
+            System.out.println("账号密码不匹配");
+        }
+    }
+}
---
+++ src/main/java/realm/MyRealm.java
@@ -0,0 +1,39 @@
+package realm;
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthenticatingRealm;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+public class MyRealm extends AuthorizingRealm {
+
+    /**
+     * 用户认证
+     * @param authenticationToken
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        UsernamePasswordToken token = (UsernamePasswordToken) authenticationToken;
+        //这里假设从数据库查出来的账号密码
+        String userName = "dm";
+        String passWord = "0616";
+        if (!token.getUsername().equals(userName)) {
+            //表示账号不存在
+            return null;
+        }
+        return new SimpleAuthenticationInfo(userName,passWord,"MyRealm");
+    }
+
+    /**
+     * 用户授权
+     * @param principalCollection
+     * @return
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+}
